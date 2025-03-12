df208e3e90dda8bfcb333dcd9f1b325f6e8037b4
Ignacio Talvi <ignaciotalvi@gmail.com>
2025-03-12 15:35:44 +0100

    Ejercicio 1 completo

---
+++ scripts.js
@@ -0,0 +1,60 @@
+const allLinks = document.querySelectorAll("a");
+
+for (let i = 0; i < allLinks.length; i++) {
+  allLinks[i].href = "";
+}
+
+// const clickDisabled = document.getElementsByTagName("a").onclick = function() {
+//     clickDisabled.removeAttribute("onclick");
+// }
+
+// const clickDisabled = document.getElementsByTagName("a");
+
+// document.getElementsByTagName("a").addEventListener ("click", function () {
+//     a.removeAttribute("onclick");
+// }
+
+// const clickDisabled = document
+//   .getElementsByTagName("a")
+//   .addEventListener("click", function () {
+//     clickDisabled.disable = true;
+//   });
+
+// const clickDisabled = document
+//   .getElementsByTagName("a")
+//   .setAttribute("disabled", true);
+
+// const allLinks = document.getElementById("span");
+// allLinks.disabled = true;
+
+// function disableClick() {
+//     var input = document.getElementById('textInput').value;
+//     var button = document.getElementById('submitButton');
+//     button.disabled = input === '';
+//   }
+
+// allLinks.disabled = true;
+
+// document.getElementsByTagName("a").disabled = "disabled";
+
+//clickDisabled.disabled = true;
+
+// const linkDisabled = document.getElementsByTagName("a");
+// function clickDisabled() {
+//   linkDisabled.removeAttribute("onclick");
+// }
+
+// for (let i = 0; i < clickDisabled.length; i++) {
+//     clickDisabled.removeAttribute("onclick");
+// }
+
+// const clickDisabled = document.getElementsByTagName("link").onclick = function() {
+//     clickDisabled.removeAttribute("onclick");
+// }
+
+// for(var i=0; i<clickDisabled.length; i++){
+//     clickDisabled[i].addEventListener("click", clickFunction);
+//   }
+
+// document.getElementById("btn").onclick = function(){
+//     document.getElementById("chip-filterTags").ej2_instances[0].removeEventListener("click"); }
